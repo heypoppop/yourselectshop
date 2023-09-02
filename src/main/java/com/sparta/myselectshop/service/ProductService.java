cc100af144f68821adc65b4af682fbc999f1a0b3
heypop <hey@herenas.com>
2023-09-02 14:57:08 +0900

    상품 페이징 및 정렬

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -5,9 +5,14 @@ import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.entity.Product;
 import com.sparta.myselectshop.entity.User;
+import com.sparta.myselectshop.entity.UserRoleEnum;
 import com.sparta.myselectshop.naver.dto.ItemDto;
 import com.sparta.myselectshop.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,7 +26,7 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-    public static final int MIN_PRICE = 100;
+    public static final int MIN_MY_PRICE = 100;
 
     public ProductResponseDto createProduct(ProductRequestDto requestDto, User user) {
         Product product = productRepository.save(new Product(requestDto, user));
@@ -31,8 +36,8 @@ public class ProductService {
     @Transactional
     public ProductResponseDto updateProduct(Long id, ProductMypriceRequestDto requestDto) {
         int myprice = requestDto.getMyprice();
-        if(myprice < MIN_PRICE) {
-            throw new IllegalArgumentException("유효하지 않은 관심 가격입니다. 최소" + MIN_PRICE + "원 이상으로 설정해 주세요.");
+        if(myprice < MIN_MY_PRICE) {
+            throw new IllegalArgumentException("유효하지 않은 관심 가격입니다. 최소" + MIN_MY_PRICE + "원 이상으로 설정해 주세요.");
         }
         Product product = productRepository.findById(id).orElseThrow(
                 () -> new NullPointerException("해당 상품을 찾을 수 없습니다.")
@@ -43,13 +48,23 @@ public class ProductService {
         return new ProductResponseDto(product);
     }
 
-    public List<ProductResponseDto> getProducts(User user) {
-        List<Product> productList = productRepository.findAllByUser(user);
-        List<ProductResponseDto> responseDtoList = new ArrayList<>();
-        for (Product product : productList) {
-            responseDtoList.add(new ProductResponseDto(product));
+    public Page<ProductResponseDto> getProducts(User user, int page, int size, String sortBy, boolean isAsc) {
+        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Sort sort = Sort.by(direction, sortBy);
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        UserRoleEnum userRoleEnum = user.getRole();
+
+        Page<Product> productList;
+
+        if (userRoleEnum == UserRoleEnum.ADMIN) {
+            productList = productRepository.findAll(pageable);
+        } else {
+            productList = productRepository.findAllByUser(user, pageable);
         }
-        return responseDtoList;
+
+
+        return productList.map(ProductResponseDto::new);
     }
 
     @Transactional
@@ -60,12 +75,5 @@ public class ProductService {
         product.updateByItemDto(itemDto);
     }
 
-    public List<ProductResponseDto> getAllProducts() {
-        List<Product> productList = productRepository.findAll();
-        List<ProductResponseDto> responseDtoList = new ArrayList<>();
-        for (Product product : productList) {
-            responseDtoList.add(new ProductResponseDto(product));
-        }
-        return responseDtoList;
-    }
+
 }
